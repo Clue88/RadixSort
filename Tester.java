0ba1057dce541bf3120cc11fa593255d7a0d616d
Christopher Liu <christopherliu@sorcero.com>
2021-01-11 13:41:49 -0500

    Update gitignore and add tester framework

---
+++ Tester.java
@@ -0,0 +1,74 @@
+/******************************************************************************
+ *  Compilation:  javac Tester.java
+ *  Execution:    java Tester [DEBUG]
+ *
+ *  DEBUG: Whether to add extra outputs (seed, error output). Defaults true.
+ ******************************************************************************/
+
+public class Tester {
+    public static int ERR = 0;
+    public static boolean DEBUG = true;
+    public static void main(String[] args) {
+        if (args.length > 0 && Boolean.parseBoolean(args[0]) == false) DEBUG = false;
+        // String test = "";
+
+        
+
+        if (ERR == 0) System.out.println("All good!");
+        else if (ERR == 1) System.out.println("Uh oh... 1 error found.");
+        else System.out.println("Uh oh... " + ERR + " errors found.");
+    }
+
+    public static void check(String test, Object actual, Object expected) {
+        if (actual == null || expected == null) {
+            if (actual != null) {
+                System.out.println("Failure on " + test + ": Expected \"" +
+                                expected + "\", got \"" + actual + "\".");
+                ERR++;
+            }
+            return;
+        }
+
+        if (!actual.equals(expected)) {
+            System.out.println("Failure on " + test + ": Expected \"" +
+                                expected + "\", got \"" + actual + "\".");
+            ERR++;
+        }
+    }
+
+    public static void check(String test, Object actual, Object expected, int seed) {
+        if (actual == null || expected == null) {
+            if (actual != null) {
+                System.out.print("Failure on " + test + ": Expected \"" +
+                                expected + "\", got \"" + actual + "\".");
+                if (DEBUG) System.out.println(" Seed: " + seed);
+                else System.out.println();
+                ERR++;
+            }
+            return;
+        }
+        
+        if (!actual.equals(expected)) {
+            System.out.print("Failure on " + test + ": Expected \"" +
+                                expected + "\", got \"" + actual + "\".");
+            if (DEBUG) System.out.println(" Seed: " + seed);
+            else System.out.println();
+            ERR++;
+        }
+    }
+
+    public static void except(String test, RuntimeException e) {
+        System.out.println("Failure on " + test + ": RuntimeException");
+        if (DEBUG) System.out.println(e.toString());
+        ERR++;
+    }
+
+    public static void noException(String test, String expected) {
+        System.out.println("Failure on " + test + ": Expected " + expected);
+        ERR++;
+    }
+
+    public static void nothing(Object... nothings) {
+        return;
+    }
+}
